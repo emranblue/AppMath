71ff29d5aa5c218cb5a6e6c2135e6cd2c3a37bcc
emranblue <emran.blue.120@gmail.com>
2021-07-06 20:02:08 +0600

    updated

--- diff.f90
+++ diff.f90
@@ -1,8 +1,37 @@
-        real function diff(func,a,dx) result(z)
+        real function diff_n(func,a,dx) result(z)
           implicit none
           real,external::func
           real,intent(in)::a,dx
           z=(func(a+dx)-func(a))/dx
           end function
+
+
+          real function diff_te(f,a,dx) result(z)
+            implicit none
+            real,external::f
+            real,intent(in)::a,dx
+            z=(1/(2*dx))*(-3*f(a)+4*f(a+dx)-f(a+2*dx))
+            end function
+
+        real function diff_tm(f,a,dx) result(z)
+          implicit none
+          real,external::f
+          real,intent(in)::a,dx
+          z=(1/(2*dx))*(f(a+dx)-f(a-dx))
+          end function
+
+      real function diff_fm(f,a,dx) result(z)
+        implicit none
+        real,external::f
+        real,intent(in)::a,dx
+        z=(1/(12*dx))*(f(a-2*dx)-8*f(a-dx)+8*f(a+dx)-f(a+2*dx))
+        end function
+
+     real function diff_fe(f,a,dx) result(z)
+        implicit none
+        real,external::f
+        real,intent(in)::a,dx
+        z=(1/(12*dx))*(-25*f(a)+48*f(a+dx)-36*f(a+2*dx)+16*f(a+3*dx)-3*f(a+4*dx))
+        end function
 
 
--- main.f90
+++ main.f90
@@ -2,12 +2,16 @@ program main
       implicit none
       real::a,b,dx
       real,external::f
-      real::trap_int,simp_int,simp_te_int,diff
+      real::trap_int,simp_int,simp_te_int,diff_n,diff_te,diff_tm,diff_fe,diff_fm
       read(*,*)a,b,dx
       write(*,*)"with trapizoid",trap_int(f,a,b,dx)
       write(*,*)"with simpson",simp_int(f,a,b,dx)
       write(*,*)"with simpson three eight",simp_te_int(f,a,b,dx)
-      write(*,*)"diff is ",diff(f,a,dx)
+      write(*,*)"diff n is ",diff_n(f,a,dx)
+      write(*,*)"diff te is ",diff_te(f,a,dx)
+      write(*,*)"diff tm is ",diff_tm(f,a,dx)
+      write(*,*)"diff fe is ",diff_fe(f,a,dx)
+      write(*,*)"diff fm is ",diff_fm(f,a,dx)
       end program
 
 
@@ -15,5 +19,5 @@ program main
 real function f(x)
       implicit none
       real::x
-      f=x**2
+      f=x**2-x+1
 end function
